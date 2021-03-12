547798b107ccaafa21f2df8e2e0573f3a1af9e00
Eduardo Sayans <edusayans@gmail.com>
2021-03-12 19:34:29 -0300

    Aula

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import keys from './keys';
 
 export default function App() {
+
+  const endPoint = "https://api.openweathermap.org/data/2.5/forecast?lang=pt_br&units=metric&q=";
+  const apiKey = keys.weatherMapApiKey;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.entrada}>
+        <TextInput
+          style={styles.nomeCidade}
+          placeholder="Digite o nome de uma cidade"
+        />
+        <Button
+          title="Ok"
+        />
+      </View>
+      <FlatList
+      
+      />
     </View>
-  );
-}
+    );
+  }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 40,
+    flexDirection: 'column',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  nomeCidade: {
+    padding: 10,
+    borderBottomColor: '#BB96F3',
+    borderBottomWidth: 2,
+    textAlign: 'left',
+    flexGrow: 0.9
+  },
+  entrada: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 8
   },
 });
